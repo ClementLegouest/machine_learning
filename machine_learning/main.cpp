c00e64e0b904a1256cb331b37ccfa84941faafa9
ClementLegouest <clement.legouest@gmail.com>
2020-04-03 11:45:13 +0200

    [evo] clean

--- machine_learning/main.cpp
+++ machine_learning/main.cpp
@@ -11,6 +11,7 @@
 
 int main(int argc, char *argv[]) {
 	
+	/*
 	// Hardcoded matrix to remove for final version
 	Eigen::MatrixXd x(8, 4);
 	Eigen::MatrixXd y(8, 1);
@@ -19,11 +20,22 @@ int main(int argc, char *argv[]) {
 		187, 0, 0, 1, 185, 1, 0, 0, 166, 0, 0, 1, 177, 0, 1, 0;
 	y << 24, 22, 23, 25, 23, 26, 25, 32;
 	// End of hardcoded matrix to remove for final version
+	*/
 
-	displayAMatrix(x, "x");
-	displayAMatrix(y, "y");
-	displayAMatrix(W(x, y), "w(x, y)");
-	displayAMatrix(iterrative_W(10000, x, y, 0.01, 1), "iterrative_W");
+	// Hardcoded matrix to remove for final version
+	Eigen::MatrixXd matrixX(300, 1);
+	Eigen::MatrixXd matrixY(300, 1);
+
+	for (int i = 0; i < matrixX.cols(); i++) {
+		matrixX(i, 0) = i + 1;
+		matrixY(i, 0) = getARandom(0.00, 1.00);
+	}
+	// End of hardcoded matrix to remove for final version
+
+	displayAMatrix(matrixX, "x");
+	displayAMatrix(matrixY, "y");
+	displayAMatrix(W(matrixX, matrixY), "w(x, y)");
+	displayAMatrix(iterrative_W(10000, matrixX, matrixY, 0.01, 1), "iterrative_W");
 	// displayAMatrix(W(x, y) - iterrative_W(2000, x, y, 0.01, 1), "différence W - Iterrative_W");
 
 	std::cin.get();
